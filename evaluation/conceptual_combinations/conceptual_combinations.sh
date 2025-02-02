5e9213c691dab99440677de3f3be62b2452e8c68
tony10101105 <b08901133@ntu.edu.tw>
2025-02-02 15:24:27 +0800

    add missing files

---
+++ evaluation/conceptual_combinations/conceptual_combinations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MODEL_FILE="all_models.txt"
+PYTHON_CMD="lm_eval --model hf --model_args pretrained=PLACEHOLDER,dtype=float16,parallelize=True,trust_remote_code=True --num_fewshot 2 --tasks bigbench_conceptual_combinations_multiple_choice\
+ --device cuda --batch_size auto --output_path /home/b08901133/lm-evaluation-harness/eval_out/conceptual_combinations --log_samples"
+
+while IFS= read -r model_name
+do
+    # Check if the model name is not empty
+    if [ -n "$model_name" ]; then
+        # Replace PLACEHOLDER with the actual model name
+        cmd="${PYTHON_CMD//PLACEHOLDER/$model_name}"
+        
+        echo "Running command: $cmd"
+        # Execute the modified command
+        eval "$cmd"
+    fi
+done < "$MODEL_FILE"
